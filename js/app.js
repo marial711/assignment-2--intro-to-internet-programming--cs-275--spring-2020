9b9b2a6af97544eb6db5b0a2825ef3eb3ba9bcdd
marial711 <marialeyva782@gmail.com>
2020-04-18 03:29:00 -0400

    Added functions to change Sprouted Rice Quanities

--- js/app.js
+++ js/app.js
@@ -1,37 +1,60 @@
 //Loads the page without recipes
 window.onload = () => {
     document.getElementById(`sprouted`).style.display = `none`;
-    document.getElementById(`rice`).style.display = `none`;
+    document.getElementById(`recipe-1`).style.display = `none`;
 };
 //Getting serving size
 function getInputValue(){
     let size = parseInt(document.getElementById(`quanity`).value);
     return size;
 }
-//Funcitons to retrieve amounts for white rice
-function cupsOfWhiteRice(){
-    let riceAmount = parseInt(document.getElementById(`rice-1`).innerHTML);
+//Functions to retrieve amounts for white rice
+function cupsOfWater(){
+    let riceAmount = parseInt(document.getElementById(`water-1`).innerHTML);
     return riceAmount;
 }
 function cupsOfOil(){
     let oilAmount = parseInt(document.getElementById(`oil-1`).innerHTML);
     return oilAmount;
 }
-//test
-console.log(cupsOfWhiteRice());
-console.log(cupsOfOil());
+//Functions retrieving amounts for sprouted riceAmountfunction cupsOfWater(){
+function cupsOfWaterSprouted(){
+    let riceAmount = parseInt(document.getElementById(`water-2`).innerHTML);
+    return riceAmount;
+}
+function cupsOfOilSprouted(){
+    let oilAmount = parseInt(document.getElementById(`oil-2`).innerHTML);
+    return oilAmount;
+}
+//Rewriting Recipe
+function rewriteRecipe(){
+    let origWater= cupsOfWater(),
+        servingSize= getInputValue(),
+        newWater = origWater*servingSize;
+    document.getElementById(`water-1`).innerHTML = newWater;
+    let origOil = cupsOfOil(),
+        newOil = origOil * servingSize;
+    document.getElementById(`oil-1`).innerHTML = newOil;
+    let sproutedWater = cupsOfWaterSprouted(),
+        newSprouted = sproutedWater*servingSize;
+    document.getElementById(`water-2`).innerHTML = newSprouted;
+    let sproutedOil = cupsOfOilSprouted(),
+        newSproutedOil = sproutedOil * servingSize;
+    document.getElementById(`oil-2`).innerHTML = newSproutedOil;
+}
 //Once button gets clicked it stores value in getInputValue()
 document.getElementById(`enter`).onclick = function(){
-    console.log(getInputValue());
+    getInputValue();
+    rewriteRecipe();
 };
 //Displays recipe based on button
 document.getElementById(`white-rice`).onclick = function (){
-    if(document.getElementById(`rice`).style.display === `none`){
-        document.getElementById(`rice`).style.display = `block`;
+    if(document.getElementById(`recipe-1`).style.display === `none`){
+        document.getElementById(`recipe-1`).style.display = `block`;
         document.getElementById(`white-rice`).innerHTML = `Close`;
     }
-    else if(document.getElementById(`rice`).style.display === `block`){
-        document.getElementById(`rice`).style.display = `none`;
+    else if(document.getElementById(`recipe-1`).style.display === `block`){
+        document.getElementById(`recipe-1`).style.display = `none`;
         document.getElementById(`white-rice`).innerHTML = `White Rice`;
     }
 };
