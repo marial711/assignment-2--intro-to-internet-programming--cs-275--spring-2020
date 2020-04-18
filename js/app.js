72a4aa5438d0eeb6202ca7748d2ffd9310084d02
marial711 <marialeyva782@gmail.com>
2020-04-18 01:39:57 -0400

    Added functions to retrieve original servings of recipe

--- js/app.js
+++ js/app.js
@@ -3,6 +3,23 @@ window.onload = () => {
     document.getElementById(`sprouted`).style.display = `none`;
     document.getElementById(`rice`).style.display = `none`;
 };
+//Getting serving size
+
+//Funcitons to retrieve white rice amounts
+function cupsOfWhiteRice(){
+    let riceAmount = parseInt(document.getElementById(`rice-1`).innerHTML);
+    return riceAmount;
+}
+function cupsOfOil(){
+    let oilAmount = parseInt(document.getElementById(`oil-1`).innerHTML);
+    return oilAmount;
+}
+//test
+console.log(cupsOfWhiteRice());
+console.log(cupsOfOil());
+//Once button gets clicked it stores value in getInputValue()
+document.getElementById(`enter`).onclick = function(){
+};
 //Displays recipe based on button
 document.getElementById(`white-rice`).onclick = function (){
     if(document.getElementById(`rice`).style.display === `none`){
