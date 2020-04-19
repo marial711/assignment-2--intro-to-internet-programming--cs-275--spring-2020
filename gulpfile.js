8a7e72a7c6225c476a4129c69b6c8d0794127ca7
marial711 <marialeyva782@gmail.com>
2020-04-19 16:13:43 -0400

    Had to restart

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,1 @@
-const{ src } = require(`gulp`);
-const htmlValidator = require(`gulp-html`);
 
-let validateHTML = () => {
-    return src(`html-files/*.html`)
-        .pipe(htmlValidator());
-};
-
-exports.validateHTML = validateHTML;
