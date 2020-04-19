dbdc7bbae75c40341411ed479e6fa5da76560faf
marial711 <marialeyva782@gmail.com>
2020-04-19 16:45:50 -0400

    HTML validator and compressor added

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,19 @@
+const { src, dest } = require(`gulp`);
+const htmlCompressor = require(`gulp-htmlmin`);
 
+let compressHTML = () => {
+    return src(`html/*.html`)
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`compressed-html/`));
+}
+
+exports.compressHTML = compressHTML;
+
+const htmlValidator = require(`gulp-html`);
+
+let validateHTML = () => {
+    return src(`html/*.html`)
+        .pipe(htmlValidator());
+};
+
+exports.validateHTML = validateHTML;
