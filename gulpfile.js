99acbe56743b5a93d252dcde2545f6d5f1d1826b
marial711 <marialeyva782@gmail.com>
2020-04-20 13:30:53 -0400

    Updated transpileJSFor Prod task

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,65 @@
-const { src, dest } = require(`gulp`);
+const { src, dest, watch} = require(`gulp`);
 const htmlCompressor = require(`gulp-htmlmin`);
-let compressHTML = () => {
-    return src(`html/*.html`)
-        .pipe(htmlCompressor({collapseWhitespace: true}))
-        .pipe(dest(`compressed-html/`));
-};
-exports.compressHTML = compressHTML;
-
 const htmlValidator = require(`gulp-html`);
+const babel = require(`gulp-babel`);
+const jsCompressor = require(`gulp-uglify`);
+const jsLinter = require(`gulp-eslint`);
+const cssLinter = require(`gulp-stylelint`);
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
+
 let validateHTML = () => {
-    return src(`html/*.html`)
+    return src(`html/index.html`)
         .pipe(htmlValidator());
 };
-exports.validateHTML = validateHTML;
 
-const jsLinter = require(`gulp-eslint`);
+let compressHTML = () => {
+    return src(`html/index.html`)
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let transpileJSForDev = () => {
+    return src(`js/app.js`)
+        .pipe(babel())
+        .pipe(dest(`js`));
+};
+
+let transpileJSForProd = () => {
+    return src(`js/app.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+
 let lintJS = () => {
-    return src(`js/*.js`)
-        .pipe(jsLinter())
+    return src(`js/app.js`)
+        .pipe(jsLinter({
+            "parserOptions": {
+                "ecmaVersion": 2018,
+                "sourceType": `module`
+            },
+            "rules": {
+                "no-console": 0,
+                "no-debugger": 0,
+                "indent": [2, 4, {"SwitchCase": 1}],
+                " quotes": [2, `backtick`],
+                "linebreak-style": [2, `unix`],
+                "semi": [2, `always`],
+                "max-len": [2, 85, 4]
+            },
+            "env": {
+                "es6": true,
+                "node": true,
+                "browser": true
+            },
+            extends: `eslint:recommended`
+        }))
         .pipe(jsLinter.formatEach(`compact`, process.stderr));
 };
-exports.lintJS = lintJS;
 
-const cssLinter = require(`gulp-stylelint`);
 let lintCSS = () => {
-    return src(`css/*.css`)
+    return src(`css/style.css`)
         .pipe(cssLinter({
             failAfterError: true,
             reporters: [
@@ -32,4 +67,25 @@ let lintCSS = () => {
             ]
         }));
 };
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 0,
+        server: {
+            baseDir: `prod`
+        }
+    });
+    watch([
+        `./*.html`,
+        `./js/**/*.js`
+    ]).on(`change`, reload);
+};
+
+exports.compressHTML = compressHTML;
+exports.validateHTML = validateHTML;
 exports.lintCSS = lintCSS;
+exports.lintJS = lintJS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.transpileJSForProd = transpileJSForProd;
+exports.serve = serve;
--- js/app.js
+++ js/app.js
@@ -1,88 +1,99 @@
 //Loads the page without recipes
-window.onload = () => {
+window.onload = function () {
     document.getElementById(`sprouted`).style.display = `none`;
     document.getElementById(`recipe-1`).style.display = `none`;
-};
-//Getting serving size
-function getInputValue(){
-    let size = parseFloat(document.getElementById(`quanity`).value);
+}; //Getting serving size
+
+
+function getInputValue() {
+    var size = parseFloat(document.getElementById(`quanity`).value);
     return size;
-}
-//Functions to retrieve amounts for white rice
-function cupsOfWater(){
-    let waterAmount = parseFloat(document.getElementById(`water-1`).innerHTML);
+} //Functions to retrieve amounts for white rice
+
+
+function cupsOfWater() {
+    var waterAmount = parseFloat(document.getElementById(`water-1`).innerHTML);
     return waterAmount;
 }
-function cupsOfOil(){
-    let oilAmount = parseFloat(document.getElementById(`oil-1`).innerHTML);
+
+function cupsOfOil() {
+    var oilAmount = parseFloat(document.getElementById(`oil-1`).innerHTML);
     return oilAmount;
 }
-function cupsOfRice(){
-    let riceAmount = parseFloat(document.getElementById(`rice`).innerHTML);
+
+function cupsOfRice() {
+    var riceAmount = parseFloat(document.getElementById(`rice`).innerHTML);
+    return riceAmount;
+} //Functions retrieving amounts for sprouted rice{
+
+
+function cupsOfWaterSprouted() {
+    var riceAmount = parseFloat(document.getElementById(`water-2`).innerHTML);
     return riceAmount;
 }
-//Functions retrieving amounts for sprouted rice{
-function cupsOfWaterSprouted(){
-    let riceAmount = parseFloat(document.getElementById(`water-2`).innerHTML);
-    return riceAmount;
-}
-function cupsOfOilSprouted(){
-    let oilAmount = parseFloat(document.getElementById(`oil-2`).innerHTML);
+
+function cupsOfOilSprouted() {
+    var oilAmount = parseFloat(document.getElementById(`oil-2`).innerHTML);
     return oilAmount;
 }
-function cupsOfSproutedRice(){
-    let amount = document.getElementById(`fraction`).innerHTML;
+
+function cupsOfSproutedRice() {
+    var amount = document.getElementById(`fraction`).innerHTML;
     amount = 1.25;
     return amount;
-}
-//Rewriting Recipe
-function rewriteRecipe(){
-    console.log(typeof cupsOfSproutedRice() + ` ` + cupsOfSproutedRice());
-    let origWater= cupsOfWater(),
-        servingSize= getInputValue(),
-        newWater = origWater*servingSize;
+} //Rewriting Recipe
+
+
+function rewriteRecipe() {
+    console.log(typeof(cupsOfSproutedRice()) + ` ` + cupsOfSproutedRice());
+    var origWater = cupsOfWater(),
+        servingSize = getInputValue(),
+        newWater = origWater * servingSize;
     document.getElementById(`water-1`).innerHTML = newWater;
-    let origOil = cupsOfOil(),
+    var origOil = cupsOfOil(),
         newOil = origOil * servingSize;
     document.getElementById(`oil-1`).innerHTML = newOil;
-    let sproutedWater = cupsOfWaterSprouted(),
-        newSprouted = sproutedWater*servingSize;
+    var sproutedWater = cupsOfWaterSprouted(),
+        newSprouted = sproutedWater * servingSize;
     document.getElementById(`water-2`).innerHTML = newSprouted;
-    let sproutedOil = cupsOfOilSprouted(),
+    var sproutedOil = cupsOfOilSprouted(),
         newSproutedOil = sproutedOil * servingSize;
     document.getElementById(`oil-2`).innerHTML = newSproutedOil;
-    let whiteRice = cupsOfRice(),
-        newRice;
-    if(servingSize !== 1){
+    var whiteRice = cupsOfRice(),
+        newRice = void 0;
+
+    if (servingSize !== 1) {
         newRice = whiteRice * servingSize;
         document.getElementById(`rice`).innerHTML = newRice + ` cups`;
     }
-    let sproutedRice = cupsOfSproutedRice(),
+
+    var sproutedRice = cupsOfSproutedRice(),
         newSproutedAmount = sproutedRice * servingSize;
     document.getElementById(`fraction`).innerHTML = newSproutedAmount;
-}
-//Once button gets clicked it stores value in getInputValue()
-document.getElementById(`enter`).onclick = function(){
+} //Once button gets clicked it stores value in getInputValue()
+
+
+document.getElementById(`enter`).onclick = function () {
     getInputValue();
     rewriteRecipe();
-};
-//Displays recipe based on button
-document.getElementById(`white-rice`).onclick = function (){
-    if(document.getElementById(`recipe-1`).style.display === `none`){
+}; //Displays recipe based on button
+
+
+document.getElementById(`white-rice`).onclick = function () {
+    if (document.getElementById(`recipe-1`).style.display === `none`) {
         document.getElementById(`recipe-1`).style.display = `block`;
         document.getElementById(`white-rice`).innerHTML = `Close`;
-    }
-    else if(document.getElementById(`recipe-1`).style.display === `block`){
+    } else if (document.getElementById(`recipe-1`).style.display === `block`) {
         document.getElementById(`recipe-1`).style.display = `none`;
         document.getElementById(`white-rice`).innerHTML = `White Rice`;
     }
 };
-document.getElementById(`california-rice`).onclick = function (){
-    if(document.getElementById(`sprouted`).style.display === `none`){
+
+document.getElementById(`california-rice`).onclick = function () {
+    if (document.getElementById(`sprouted`).style.display === `none`) {
         document.getElementById(`sprouted`).style.display = `block`;
         document.getElementById(`california-rice`).innerHTML = `Close`;
-    }
-    else if(document.getElementById(`sprouted`).style.display === `block`){
+    } else if (document.getElementById(`sprouted`).style.display === `block`) {
         document.getElementById(`sprouted`).style.display = `none`;
         document.getElementById(`california-rice`).innerHTML = `California Rice`;
     }
